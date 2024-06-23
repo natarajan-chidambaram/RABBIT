a8f58bea60bcb9834fda2b3b8782194f5c20415d
Natarajan <48755692+natarajan-chidambaram@users.noreply.github.com>
2024-06-24 00:47:40 +0200

    updated description

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ __description__ = 'RABBIT - Rabbit is an Activity Based Bot Identification Tool 
 __long_description__ = 'This tool accepts the name of a contributor, a GitHub API key and its associated account name and computes its output in four steps.\\\
 The first step consists of extracting the public events performed by contributors in GitHub repositories. This step results in a set of events.\\\
 The second step identifies activities (belonging to 24 different activity types) performed by the contributor in GitHub repositories. If the number of activties is less than the maximum number of activities, then more events will be collected again. If the total number of activities (from all possible events) are less than specified minimum number of events, then the toll will exit with a message.\\\
-The third step constitutes of identifying the contributor behavioural features, namely, mean number of activities per activity type, number of activity types, median time between consecutive activities of different types, number of owners of repositories contributed to, Gini inequality of duration of consecutive activities and mean number of activities per repository.\\\
-The forth step simply applies the BIMBAS model that we trained on 140K activities perofrmed by 306 bots and 532 human contributors in GitHub repositories and gives the probability that a contributor is a bot.'
+The third step constitutes of identifying 38 contributor behavioural features, such as mean number of activities per activity type, number of activity types, median time between consecutive activities of different types, number of owners of repositories contributed to, Gini inequality of duration of consecutive activities and mean number of activities per repository.\\\
+The forth step simply applies the BIMBAS model that we trained on 337K activities performed by 1035 bots and 1115 human contributors in GitHub repositories and gives the probability that a contributor is a bot.'
 __classifiers__=[
         'Development Status :: 1 - Beta',
         'Intended Audience :: Developers',
